fcbcaf6fdc8772b59f1887d70544f9801003b36e
maremaric <markoreus888@gmail.com>
2022-06-20 17:07:48 +0200

    fix form

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-<div class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24">
+<x-card class="p-10 max-w-lg mx-auto mt-24">
 <header class="text-center">
     <h2 class="text-2xl font-bold uppercase mb-1">
         Create a Gig
@@ -120,5 +120,5 @@
         <a href="/" class="text-black ml-4"> Back </a>
     </div>
 </form>
-</div>
+</x-card>
 </x-layout>
